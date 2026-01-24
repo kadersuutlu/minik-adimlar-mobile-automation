ed5c1ba202f066ce2ff1c7ad90231939ea5d82a7
kadersutlu <sutlukader@gmail.com>
2026-01-24 19:26:08 +0300

    Refactor register test

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -19,11 +19,20 @@ public class RegisterPage {
 
 	private By phoneField = By.xpath("//android.widget.EditText[@text='+90 (5__) ___ __ __']");
 
+	private By passwordField = By.xpath("//android.widget.EditText[@text=\"Şifrenizi Belirleyin\"]");
+
 	private By kvkkCheckbox = AppiumBy.accessibilityId("KVKK metnini okudum, onaylıyorum.*");
 
 	private By userAgreementCheckbox = AppiumBy.accessibilityId("Kullanıcı Sözleşmesi'ni okudum, onaylıyorum.*");
 
 	private By privacyAgreementCheckbox = AppiumBy.accessibilityId("Gizlilik Sözleşmesi'ni okudum, onaylıyorum.*");
+
+	private By registerTitle = By.xpath("//android.widget.TextView[@text='Kayıt Ol']");
+	
+
+	public boolean isDisplayed() {
+		return driver.findElement(registerTitle).isDisplayed();
+	}
 
 	private By continueButton = AppiumBy.accessibilityId("Devam Et");
 
@@ -39,6 +48,10 @@ public class RegisterPage {
 		driver.findElement(phoneField).sendKeys(phone);
 	}
 
+	public void enterPassword(String password) {
+		driver.findElement(passwordField).sendKeys(password);
+	}
+
 	public void acceptAgreements() {
 		driver.findElement(kvkkCheckbox).click();
 		driver.findElement(userAgreementCheckbox).click();
@@ -49,10 +62,11 @@ public class RegisterPage {
 		driver.findElement(continueButton).click();
 	}
 
-	public void register(String name, String email, String phone) {
+	public void register(String name, String email, String phone, String password) {
 		enterName(name);
 		enterEmail(email);
 		enterPhone(phone);
+		enterPassword(password);
 		acceptAgreements();
 		tapContinue();
 	}
--- src/test/java/tests/RegisterTest.java
+++ src/test/java/tests/RegisterTest.java
@@ -1,19 +1,42 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
 import base.BaseTest;
 import pages.RegisterPage;
 
 public class RegisterTest extends BaseTest {
 
+	@Test
 	public void testSuccessfulRegister() {
 
 		System.out.println("Register test started...");
 
 		RegisterPage registerPage = new RegisterPage(driver);
 
-		registerPage.register("Test Kullanıcı", "test1@mail.com", "5554443322");
+		registerPage.register("Test Kullanıcı", "test1@mail.com", "5554443322", "Test123");
 
 		System.out.println("Register flow completed.");
+
+	}
+
+	@Test
+	public void shouldNavigateToLoginAfterSuccessfulRegister() {
+
+	}
+
+	@Test
+	public void shouldNotProceesWhenEmailIsInvalid() {
+
+	}
+
+	@Test
+	public void shouldShowErrorWhenAgreementsNotAccepted() {
+
+	}
+
+	@Test
+	public void shouldDisableContinueButtonWhenRequiredFieldsEmpty() {
 
 	}
 }
